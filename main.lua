a7f6fee62a82847199fa101b64aa51dd3a9c9b73
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-05-18 23:53:15 +0200

    feat: implement buttons

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 local love = require "love"
 local Enemy = require "Enemy"
+local button = require "Button"
 
 local game = {
   difficulty = 1,
   state = {
-    menu = false,
+    menu = true,
     paused = false,
-    running = true,
+    running = false,
     ended = false,
   },
 }
@@ -19,11 +20,31 @@ local player = {
 
 local enemies = {}
 
+local buttons = {
+  menu_state = {},
+}
+
 function love.load()
   love.window.setTitle "Save The Ball"
   love.mouse.setVisible(false)
 
+  buttons.menu_state.play_game = button("Play Game", nil, nil, 150, 40)
+  buttons.menu_state.settings = button("Settings", nil, nil, 150, 40)
+  buttons.menu_state.exit_game = button("Exit", love.event.quit, nil, 150, 40)
+
   table.insert(enemies, 1, Enemy())
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+  if not game.state["running"] then
+    if button == 1 then
+      if game.state["menu"] then
+        for index in pairs(buttons.menu_state) do
+          buttons.menu_state[index]:checkPressed(x, y, player.radius)
+        end
+      end
+    end
+  end
 end
 
 function love.update()
@@ -42,14 +63,16 @@ function love.draw()
     love.graphics.getHeight() - 30,
     love.graphics.getWidth()
   )
-  love.graphics.circle("fill", player.x, player.y, player.radius)
 
   if game.state["running"] then
     for i = 1, #enemies do
       enemies[i]:draw()
     end
-
     love.graphics.circle("fill", player.x, player.y, player.radius)
+  elseif game.state["menu"] then
+    buttons.menu_state.play_game:draw(10, 20, 17, 10)
+    buttons.menu_state.settings:draw(10, 70, 17, 10)
+    buttons.menu_state.exit_game:draw(10, 120, 17, 10)
   end
 
   if not game.state["running"] then
